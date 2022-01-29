a2c31ddeb82bcb3e4480fbf6bde6e35f5073df9b
Michael E Ershov <ershovme@gmail.com>
2022-01-29 13:16:12 +0800

    Update JSX

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,44 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import './card.css';
 
-class Card extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name: null,
-            image: ''
-        };
-        
-    }
-
-    componentDidMount() {
-        this.loadData();
-    }
-
-    loadData() {
-        this.props.rmService
-            .getCharacter(`/character/${this.props.id}`)
-            .then((character) => {
-                this.setState({
-                    name: character.name,
-                    image: character.image
-                })
-            })
-    }
-
-    render() {
-        return(
-            <div className="card">
-                <img src={this.state.image} 
-                     alt="картинка"
-                     className="card__img"/>
-                <div className="card__text">
-                    <div>{this.state.name}</div>
-                    
-                </div>
+const Card = () => {
+    return(
+        <div className="card">
+            <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" 
+                 alt="картинка"
+                 className="card__img"/>
+            <div className="card__text">
+                <div>Morty Smith</div>            
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Card;
--- src/components/row/row.tsx
+++ src/components/row/row.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import Card from '../card';
 import './row.css';
 
-const Row = (props) => {
-    const getRandomId = (min, max) => {
-        return Math.floor(Math.random() * (max - min));
-    }
-
+const Row = () => {
+    
     return(
         <div className="row">
-            <Card id={getRandomId(1, 50)} rmService={props.rmService}/>
-            <Card id={getRandomId(20, 40)} rmService={props.rmService}/>
-            <Card id={getRandomId(30, 70)} rmService={props.rmService}/>
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
     );
 }
