e2ab7497b17679bfdb586b641946976f8481372b
Michael E Ershov <ershovme@gmail.com>
2022-01-30 22:51:52 +0800

    Added props to paginator

--- src/components/paginator/paginator.tsx
+++ src/components/paginator/paginator.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './paginator.css';
 
-const Paginator = () => {
+interface IPaginatorProps {
+    pageCount: number;
+    countAfterPrev: number;
+    countBeforeNext: number;
+    page: number;
+    prevPage: number;
+    nextPage: number;
+    changePage: () => void;
+}
+
+const Paginator = (props: IPaginatorProps) => {
     return(
         <div className="paginator">
-            <div className="paginator__button paginator__button_prev">Пред.</div>
+            <div className="paginator__button paginator__button_prev"
+                 onClick={() => props.changePage()}>Пред.</div>
             <div className="paginator__button">1</div>
             <div className="paginator__button">2</div>
             <div className="paginator__button">3</div>
