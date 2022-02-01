9da7d6da34737285a6bab7f4cbdeb57c0bf6af02
Michael E Ershov <ershovme@gmail.com>
2022-02-01 21:13:32 +0800

    Update characters component

--- src/components/characters/characters.tsx
+++ src/components/characters/characters.tsx
@@ -1,26 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import { RaMAPI } from "../app/app-context";
 import { ICharacter, ICharactersProps } from "../../types/character-types";
-import { IPage, IPaginationInfo } from "../../types/paginator-types";
 import Card from "../card";
 
 const Characters = (props: ICharactersProps): JSX.Element => {
     const [characters, setCharacters] = useState<ICharacter[]>([]);
 
-    const defaultInfo: IPaginationInfo = {
-        count: 0,
-        pages: 0,
-        next: null,
-        prev: null
-    }
-
-    const [infoPagination, setInfoPagination] = useState<IPaginationInfo>(defaultInfo)
     const [numPage, setNumPage] = useState<number>(0);
 
     useEffect(() => {
         RaMAPI.getAllCharacters(numPage)
               .then((page: IPage) => {
-                  setInfoPagination(page.info);
                   setCharacters(page.results)
               })
               //console.log(characters)
