5d5801baa72ebc6240612f75ac89fa896ebfdbde
Michael E Ershov <ershovme@gmail.com>
2022-02-01 21:39:02 +0800

    Update types Page and Character

--- src/types/character-types.ts
+++ src/types/character-types.ts
@@ -1,9 +1,37 @@
+type Origin = {
+    name: string;
+    url: string;
+}
+type Location = {
+    name: string;
+    url: string;
+}
+type Episode = string[];
+
+interface PageInfo {
+    count: number;
+    pages: number;
+    next: string;
+    prev: string;
+}
+
 interface Character {
     id: number;
     name: string;
+    status: string;
     species: string;
     gender: string;
+    origin: Origin;
+    location: Location;
     image: string;
+    episode: Episode;
+    url: string;
+    created: string;
+}
+
+interface Page {
+    info: PageInfo;
+    results: Character[];
 }
 
 interface CharactersProps{
@@ -12,5 +40,7 @@ interface CharactersProps{
 
 export type {
     Character,
-    CharactersProps
+    CharactersProps,
+    PageInfo,
+    Page
 };
