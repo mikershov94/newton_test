f1c37997b2c1bc1212deb2514d8e86885109edad
Michael E Ershov <ershovme@gmail.com>
2022-02-02 09:09:43 +0800

    Created structure for updateCharacters

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -5,3 +5,9 @@ import { CharactersFailureAction,
 const CHARACTERS_REQUESTED: CharactersRequestAction = 'CHARACTERS_REQUESTED';
 const CHARACTERS_RECEIVED:  CharactersSuccessAction = 'CHARACTERS_RECEIVED';
 const CHARACTERS_FAILURE:   CharactersFailureAction = 'CHARACTERS_FAILURE';
+
+export {
+    CHARACTERS_REQUESTED,
+    CHARACTERS_RECEIVED,
+    CHARACTERS_FAILURE
+}
--- src/store/reducers/characters-reducer.ts
+++ src/store/reducers/characters-reducer.ts
@@ -1,6 +1,18 @@
+import { CHARACTERS_FAILURE, CHARACTERS_RECEIVED, CHARACTERS_REQUESTED } from "../actions";
+
 const updateCharacters(state, action) {
     switch (action.type) {
-        
+        case CHARACTERS_REQUESTED:
+            return {};
+
+        case CHARACTERS_RECEIVED:
+            return {};
+
+        case CHARACTERS_FAILURE:
+            return {};
+
+        default:
+            state;
     }
 }
 
