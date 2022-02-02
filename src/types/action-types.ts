8c8016ea7cc7604e320239770fc8384ec330ca50
Michael E Ershov <ershovme@gmail.com>
2022-02-02 12:06:25 +0800

    Update actions types

--- src/types/action-types.ts
+++ src/types/action-types.ts
@@ -1,9 +1,31 @@
-type CharactersRequestAction = string;
-type CharactersSuccessAction = string;
-type CharactersFailureAction = string;
+import { Page } from "./api-client-types";
+
+interface CharactersRequestAction {
+    type: string;
+};
+
+interface CharactersSuccessAction {
+    type: string;
+    payload: Page
+};
+interface CharactersFailureAction {
+    type: string;
+    payload: string;
+};
+
+type CharactersAction = CharactersRequestAction |
+                        CharactersSuccessAction |
+                        CharactersFailureAction;
+
+export enum CharactersActionTypes {
+    CHARACTERS_REQUESTED = 'CHARACTERS_REQUESTED',
+    CHARACTERS_RECEIVED  = 'CHARACTERS_RECEIVED',
+    CHARACTERS_FAILURE   = 'CHARACTERS_FAILURE'
+}
 
 export type {
     CharactersRequestAction,
     CharactersSuccessAction,
-    CharactersFailureAction
+    CharactersFailureAction,
+    CharactersAction
 }
