70cd6f177c849eb796fc1bebc8249595eb33a947
Michael E Ershov <ershovme@gmail.com>
2022-02-03 22:35:28 +0800

    Added saving like

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,14 +1,16 @@
-import React, { Dispatch, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { Dispatch, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import addFavorite from "../../store/action-creators/add-favorite";
 import deleteFavorite from "../../store/action-creators/delete-favorite";
 import { FavoriteActions } from "../../types/action-types";
 import { CardProps } from "../../types/card-types";
+import { FavoriteState, GlobalState } from "../../types/state-types";
 import Like from "../like";
 import './card.css';
 
 const Card = (props: CardProps): JSX.Element => {
     const [isCharacterLiked, setIsCharacterLiked] = useState(false);
+    const favorites: FavoriteState = useSelector((state: GlobalState) => state.favorites);
 
     const dispatch: Dispatch<FavoriteActions> = useDispatch();
 
@@ -16,11 +18,19 @@ const Card = (props: CardProps): JSX.Element => {
         dispatch(addFavorite(props.character.id));
         setIsCharacterLiked(true);
     };
-    
+
     const handleDislike = (): void => {
         dispatch(deleteFavorite(props.character.id));
         setIsCharacterLiked(false);
     };
+
+    useEffect(() => {
+        favorites.forEach((characterID: number) => {
+            if (characterID == props.character.id) {
+                setIsCharacterLiked(true);
+            }
+        })
+    })
 
     return(
         <div className="card">
